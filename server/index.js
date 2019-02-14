1a4b72992b4fad4d780f1736cbee01f5810b7324
Diego <waziri.diego@gmail.com>
2019-02-14 21:37:36 +0100

    [FIX] add new models

--- server/index.js
+++ server/index.js
@@ -6,8 +6,21 @@ const securityRouter = require('./routes/security');
 const app = express();
 const cors = require('cors');
 const Movie = require('./models/movie');
-
+const Serie = require('./models/serie');
+const Actor = require('./models/actor');
 app.use(cors());
+
+const actor1 = new Actor();
+actor1.firstname = "John";
+actor1.lastname = "Doe";
+actor1.dateBirthday = Date.now();
+actor1.nationality = "french";
+actor1.cityBirth = "Paris";
+actor1.picture = "picture.png";
+
+const serie1 = new Serie();
+serie1.title = "Test serie 2";
+serie1.year = 1930;
 
 const movie1 = new Movie();
 movie1.title = "Test Movie";
@@ -23,5 +36,7 @@ app.use(bodyparser.json());
 app.use(security.verifyToken);
 app.use("/", securityRouter);
 app.use('/movies', MovieRouter);
+app.use('/series', SerieRouter);
+app.use('/actors', ActorRouter);
 
 app.listen(3000, () => console.log('listening'));
