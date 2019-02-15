4385259004f167aa67799a84b4624ddb545b92c9
Kevin <kev_kevin@hotmail.fr>
2019-02-15 12:23:33 +0100

    create route post add serie

--- server/routes/series.js
+++ server/routes/series.js
@@ -5,15 +5,32 @@ const Episode = require('../models/episode');
 
 const router = express.Router();
 
+//GET ONE SERIE
 router.get('/:id', (req, res) => {
 	Serie.find({id: req.params.id}).then(data => res.send(data));
 });
 
+//ADD ONE SERIE WITH POST
+
+router.post('/', (req, res) => {
+    res.send(req.body);
+    const serie1 = new Serie();
+	serie1.name = req.body.name;
+	serie1.number_of_episodes = req.body.number_of_episodes;
+    serie1.number_of_seasons = req.body.number_of_seasons;
+    serie1.poster_path = req.body.poster_path;
+    serie1.country = req.body.country;
+	serie1.save(function(error, result){
+	});
+	res.send(serie1);
+})
+
+//GET ALL SERIES
 router.get('/', (req, res) => {
     Serie.find().then(data => res.send(data));
 });
 
-// Season
+// GET Season
 router.get('/:serieid/season/:seasonid', (req, res) => {
     Season.find({serie_id: req.params.serieid, season_number: req.params.seasonid}).then(data => res.send(data));
 });
